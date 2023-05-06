db811f2c67fccbf8b5dbd899e3a3ceaf2245732f
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-05-06 00:01:52 -0300

    dataBase

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ const server = require("./app");
 const { conn } = require("../database/db");
 
 conn.sync({ force: true }).then(() => {
-  console.log("estamos conectado :)");
+  console.log("estamos conectado ");
   server.listen(3001, () => {
     console.log("Bienvenido al port 3001");
   });
