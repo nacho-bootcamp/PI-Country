adef1ea252327afeff22f3607e83b3e4722b860b
nacho745 <ignaciocardozoxd12@gmail.com>
2023-05-23 18:26:41 -0300

    modal succes

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import styles from "./Modal.module.css";
 
-const Modal = () => {
+const Modal = ({ children, modal, setModal }) => {
   return (
-    <div className={styles.Overlay}>
-      <div className={styles.Container}>
-        <div className={styles.Encabezado}>
-          <h3>TITULO</h3>
+    <>
+      {modal && (
+        <div className={styles.Overlay}>
+          <div className={styles.Container}>
+            <div className={styles.Encabezado}>
+              <h3>TITULO</h3>
+            </div>
+            <div onClick={() => setModal(!modal)} className={styles.Btn}>
+              x
+            </div>
+            {children}
+          </div>
         </div>
-        <div className={styles.Btn}></div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
