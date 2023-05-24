366ca505dbc5343485611c72e5789bf15b977728
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-05-24 16:31:21 -0300

    paginado

--- client/src/components/Paginado/Paginado.jsx
+++ client/src/components/Paginado/Paginado.jsx
@@ -5,15 +5,18 @@ const Pagination = ({ itemsPerPage, totalItems, paginate }) => {
   const pageNumbers = [];
   const [currentPage, setCurrentPage] = useState(1);
 
+  // Genera un array con los números de página disponibles
   for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
     pageNumbers.push(i);
   }
 
+  // Maneja el evento de clic en un número de página
   const handleClick = (pageNumber) => {
     setCurrentPage(pageNumber);
     paginate(pageNumber);
   };
 
+  // Maneja el evento de clic en el botón next
   const handleNext = () => {
     if (currentPage < pageNumbers.length) {
       setCurrentPage((prevPage) => prevPage + 1);
@@ -21,6 +24,7 @@ const Pagination = ({ itemsPerPage, totalItems, paginate }) => {
     }
   };
 
+  // Maneja el evento de clic en el botón back
   const handleBack = () => {
     if (currentPage > 1) {
       setCurrentPage((prevPage) => prevPage - 1);
@@ -28,6 +32,7 @@ const Pagination = ({ itemsPerPage, totalItems, paginate }) => {
     }
   };
 
+  // Genera los números de página visibles en la interfaz
   const renderPageNumbers = () => {
     const visiblePageNumbers = pageNumbers.slice(
       currentPage - 1,
@@ -49,12 +54,17 @@ const Pagination = ({ itemsPerPage, totalItems, paginate }) => {
 
   return (
     <ul className={styles.pagination}>
+      {/* Botón Back */}
       {currentPage > 1 && (
         <button className={styles.navigationButton} onClick={handleBack}>
           Back
         </button>
       )}
+
+      {/* Números de página */}
       {renderPageNumbers()}
+
+      {/* Botón Next*/}
       {currentPage < pageNumbers.length && (
         <button className={styles.navigationButton} onClick={handleNext}>
           Next
